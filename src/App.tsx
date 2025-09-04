90fe2e820bed2a7ce2b9ff2dadfeb9fad131fd7d
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-09-04 15:03:54 +0300

    chore: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css'
+import { Button } from '@/components/ui/button.tsx'
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default App
