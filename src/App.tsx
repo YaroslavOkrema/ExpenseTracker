12d77212a7a1db6ac5590cf58477dece40e67a45
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-09-04 16:03:55 +0300

    feat: add simple component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card.tsx'
 import { Button } from '@/components/ui/button.tsx'
+import { Label } from '@/components/ui/label.tsx'
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
+    <div className="flex justify-center items-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-center">Мій бюджет</CardTitle>
+          <Label>Баланс: 3500 грн</Label>
+          <div className="flex gap-4 mb-4">
+            <Label>Доходи: 5000 грн</Label>
+            <Label>Витрати: 3500 грн</Label>
+          </div>
+          <Button variant="link">Додати транзакцію</Button>
+        </CardHeader>
+        <CardContent>
+          <Label>Список транзакцій:</Label>
+        </CardContent>
+      </Card>
     </div>
   )
 }
