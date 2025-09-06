1c7d1fa2fb5123b42c212393dbfd234ff63da7be
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-09-07 01:21:34 +0300

    feat: add tabs

--- src/pages/CardPage/index.tsx
+++ src/pages/CardPage/index.tsx
@@ -9,6 +9,12 @@ import { Button } from '@/components/ui/button.tsx'
 import FormComponent from '@/components/FormComponent'
 import TableComponent from '@/components/TableComponent'
 import { useCardPage } from '@/pages/CardPage/useCardPage.ts'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/components/ui/tabs.tsx'
 
 function CardComponent() {
   const {
@@ -24,36 +30,56 @@ function CardComponent() {
 
   return (
     <div className="flex justify-center items-center">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-center">Мій бюджет</CardTitle>
-          <Label>Баланс: {balance} грн</Label>
-          <div className="flex gap-4 mb-4">
-            <Label>Доходи: {income} грн</Label>
-            <Label>Витрати: {expenses} грн</Label>
-          </div>
-          <Button
-            className="cursor-pointer"
-            variant="link"
-            onClick={toggleShowForm}
-          >
-            {showForm ? 'Закрити' : 'Додати транзакцію'}
-          </Button>
-        </CardHeader>
-        <CardContent>
-          {showForm ? (
-            <FormComponent
-              setShowForm={setShowForm}
-              setTransaction={setTransaction}
-            />
-          ) : (
-            <div>
-              <Label className="mb-4">Список транзакцій</Label>
-              <TableComponent transactions={transactions} />
-            </div>
-          )}
-        </CardContent>
-      </Card>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Tabs defaultValue="expense">
+          <TabsList>
+            <TabsTrigger value="expense">Expense Tracker</TabsTrigger>
+            <TabsTrigger value="analytics">Аналітика</TabsTrigger>
+          </TabsList>
+          <TabsContent value="expense">
+            <Card className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle className="text-center">Мій бюджет</CardTitle>
+                <Label>Баланс: {balance} грн</Label>
+                <div className="flex gap-4 mb-4">
+                  <Label>Доходи: {income} грн</Label>
+                  <Label>Витрати: {expenses} грн</Label>
+                </div>
+                <Button
+                  className="cursor-pointer"
+                  variant="link"
+                  onClick={toggleShowForm}
+                >
+                  {showForm ? 'Закрити' : 'Додати транзакцію'}
+                </Button>
+              </CardHeader>
+              <CardContent>
+                {showForm ? (
+                  <FormComponent
+                    setShowForm={setShowForm}
+                    setTransaction={setTransaction}
+                  />
+                ) : (
+                  <div>
+                    <Label className="mb-4">Список транзакцій</Label>
+                    <TableComponent transactions={transactions} />
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="analytics">
+            <Card className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle>Login to your account</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div>Soon...</div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   )
 }
