5ddf8c57ccc13f91bc031bcfaa20a3d1a52c8901
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-12-16 00:20:30 +0200

    feat: add telegram

---
+++ src/api/telegram/constanst.ts
@@ -0,0 +1,3 @@
+export const TELEGRAM_BOT_TOKEN = import.meta.env
+  .VITE_TELEGRAM_BOT_TOKEN as string
+export const TELEGRAM_CHAT_ID = import.meta.env.VITE_TELEGRAM_CHAT_ID as string
---
+++ src/utils/format-date-time/index.ts
@@ -0,0 +1,8 @@
+export const formatDateTime = (): string =>
+  new Date().toLocaleString('uk-UA', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
